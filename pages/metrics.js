e94b7f85dca3e2d9e97d826ad3f51af87f031bb5
92233500+leveredlong <92233500+leveredlong@users.noreply.github.com>
2021-11-01 18:22:38 -0700

    scaffolding v1

--- pages/metrics.js
+++ pages/metrics.js
@@ -1,90 +1,92 @@
-import React from 'react';
-import { Grid, Button, Container } from "@material-ui/core";
+import React, {useState} from 'react';
+import { Grid, Button, Stack } from "@material-ui/core";
 import useSWR from 'swr'
-import style from './metrics.module.css'
+import style from '../src/dev.module.css'
 
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-//TODO : Dynamic API routing / repositioning of SWR
 const Tile = props => {
 
-  const { title } = props
-  const { data } = useSWR('https://api.coingecko.com/api/v3/coins/rook/tickers?exchange_ids=uniswap_v2', fetcher)
-  // const { data } = useSWR('/api/test', fetcher)
-  // {data?.name}
+  const {data} = useSWR('https://api.coingecko.com/api/v3/coins/rook/tickers?exchange_ids=uniswap_v2', fetcher)
 
   return (
-    <>
-    {title}
-    <Container className={style.bottom}>
-      { data?.tickers[0].converted_last.usd }
-    </ Container>
-    </>
+      <div className={`${style.basic_tile} ${props.size? style.large_tile : ""}`}>
+        <Stack spacing={1}>
+            <div className={style.tile_header}>{props.title}</div>
+            <div className={style.tile_data}>${data?.tickers[0].converted_last.usd.toLocaleString()}</div>
+        </Stack>
+      </div>
   );
 }
 
-
-//TODO : Tile sizing logic
 const Panel = props => {
 
-  const {titles} = props
   let tilesList = [];
 
-  titles.forEach((item, i) => {
+  props.titles.forEach((item, i) => {
     tilesList.push(
-    <Grid item key={i} xs={i==0? 12 : 6} className={style.panel}>
-      <Tile key={i} title={titles[i]} />
+    <Grid item key={i} xs={props.sizes[i]? 12 : 6} >
+        <Tile title={props.titles[i]} size={props.sizes[i]} />
     </Grid>);
   });
 
-
   return (
-    <>
-      {tilesList}
-    </>
+      <Grid container spacing={0.5}>
+        {tilesList}
+      </Grid>
   );
 }
 
-//TODO : Use Tab/TabPanel?
-const Metrics = () => {
-
-  let titles = ['TOTAL VALUE LOCKED', 'TREASURY', 'DISTRIBUTED ROOK REWARDS', 'ROOK SUPPLY', 'ROOK VALUE']
-  let buttons = ['KPIs'];
+export const Metrics = props => {
 
   let buttonsList = [];
-  let panelsList = [];
+  let panel;
 
+  //tbd approach
+  const [activeTab, setActiveTab] = useState(0);
+  const handleChange = (event, newValue) => {
+    setActiveTab(newValue);
+  };
 
-  buttons.forEach((item, i) => {
-    panelsList.push(<Panel name={item} key={i} titles={titles}/>);
+  props.buttons.forEach((item, i) => {
+    panel = activeTab == i ? <Panel key={i} titles={props.titles[i]} sizes={props.sizes[i]} /> : panel;
     buttonsList.push(
-      <Grid item key={i}>
-        <Button key={i} variant="tab-button">{item}</Button>
-      </Grid>
+      <Button
+        key={i}
+        className={`${style.button_inactive} ${activeTab == i && style.button_active}`}
+        onClick={() => setActiveTab(i)}>
+        {item}
+      </Button>
     );
   });
 
 
   return (
-    <>
-      <Container className={style.container}>
-        <Grid container spacing={3}>
+      <Stack className={style.stack} spacing={4}>
+        <Stack className={style.group} direction="row" spacing={1}>
           {buttonsList}
-        </Grid>
-        <Grid container spacing={3}>
-          {panelsList}
-        </Grid>
-      </Container>
-    </>
+        </Stack>
+        {panel}
+      </Stack>
   );
 }
 
-// "&:focus, &:active:focus": {
-//   backgroundColor: COLORS.selected,
-//   borderColor: COLORS.selected,
-//   color: COLORS.text_primary,
-// }
+export async function getStaticProps() {
 
+  //temp
+  let buttons = ['KPIs', 'Coord. Game', 'Hiding Game', 'Hiding Vaults', 'Liquidity Pools'];
+  let titles = [['TOTAL VALUE LOCKED', 'TREASURY', 'DISTRIBUTED ROOK REWARDS', 'ROOK SUPPLY', 'ROOK VALUE'],
+                ['NUMBER OF AUCTIONS', 'BID ROOK', 'STAKED ROOK', 'BURNED ROOK', 'ACTIVE KEEPERS', 'ACTIVE AUCTIONS'],
+                ['LIMIT ORDERS', 'HIDING GAME', 'UNIQUE ORDERS', 'HIDING GAME VOLUME', 'DISTRIBUTED ROOK REWARDS'],
+                ['HISTORICAL ROOK APY', 'HISTORICAL SUPPLY/BORROW', 'CURRENT ROOK APY', 'TOTAL NUMBER OF VAULTS',
+                'DISTRIBUTED ROOK REWARDS', 'SAVED FROM LIQUIDATION BY JITU', 'TOTAL BORROW VOLUME', 'TOTAL SUPPLY VOLUME'],
+                ['HISTORICAL APY', 'TOTAL FEES COLLECTED', 'ROOK REWARDS DISTRIBUTED']];
+  let sizes = [[1,,,,], [,,,,1,1], [1,1,,], [1,1,,,,,,], [1,,]];
+
+  return {
+    props: {buttons,titles,sizes}
+  }
+}
 
 export default Metrics
