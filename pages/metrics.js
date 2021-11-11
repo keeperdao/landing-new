b1d4457bd5d684503bb22ce647350efb05f0d224
92233500+leveredlong <92233500+leveredlong@users.noreply.github.com>
2021-11-10 22:17:49 -0800

    components v0

--- pages/metrics.js
+++ pages/metrics.js
@@ -2,21 +2,102 @@ import React, {useState} from 'react';
 import { Grid, Button, Stack } from "@material-ui/core";
 import useSWR from 'swr'
 import style from '../src/dev.module.css'
+import { AreaChart, Area, XAxis, Tooltip, ResponsiveContainer, BarChart, Bar} from 'recharts';
+import {tvl, hgVolumeWeek, hgVolumeMonth, hgVolumeAll, kTokens} from '../src/sampleData'
 
 
+//major refactor req
 const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    console.log(payload)
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${label} : ${payload[0].value}`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const AChart = props => {
+
+  const ChartType = props.type;
+
+  const dataSet = props.data == "Week" ? hgVolumeWeek.days: props.data == "Month" ? hgVolumeMonth.days : hgVolumeAll.days
+
+
+  return (
+      <div style={{ width: "100%", height: 300 }}>
+        <ResponsiveContainer>
+          <AreaChart data={dataSet} >
+            <defs>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="30%" stopColor="#6C46D6" stopOpacity={0.5}/>
+                <stop offset="85%" stopColor="#6C46D60D" stopOpacity={0}/>
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="name" />
+            <Tooltip  />
+            <Area type="monotone" dataKey="daily_volume_usd" stroke="#8884d8" fillOpacity={1} fill="url(#colorPv)" />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    );
+}
+
+const BChart = props => {
+
+  const ChartType = props.type;
+
+  const dataSet = props.data == "Week" ? hgVolumeWeek.days: props.data == "Month" ? hgVolumeMonth.days : hgVolumeAll.days
+
+  return (
+      <div style={{ width: "100%", height: 300 }}>
+        <ResponsiveContainer>
+          <BarChart data={dataSet} >
+            <defs>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="30%" stopColor="#6C46D6" stopOpacity={0.5}/>
+                <stop offset="85%" stopColor="#6C46D60D" stopOpacity={0}/>
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="name" />
+            <Tooltip  />
+            <Bar type="monotone" dataKey="daily_volume_usd" stroke="#8884d8" fillOpacity={1} fill="url(#colorPv)" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    );
+}
 
 const Tile = props => {
 
   const {data} = useSWR('https://api.coingecko.com/api/v3/coins/rook/tickers?exchange_ids=uniswap_v2', fetcher)
 
+  const [activeTab, setActiveTab] = useState("Week");
+  const handleChange = (event, newValue) => {
+    setActiveTab(newValue);
+  };
+
+  let numberUsers = "5,375"
+  const text = <p className={style.info_data}>To date {numberUsers} users have saved Ξ302,5 in gas and earned 5,166 ROOK by using the Hiding Game for limit orders</p>
+
+  let test = props.type ? props.type == "Area" ? <AChart type={props.type} data={activeTab}/> : props.type == "Bar" ? <BChart type={props.type} data={activeTab} /> : props.type == "Info" ? text : "" : ""
+  let filter = props.type == "Area" | props.type == "Bar"? <Stack direction="row" spacing={1} justify-content="flex-end"> <div className={style.tile_header}>{props.title}</div> <Button className={style.button_inactive} onClick={() => setActiveTab("Week")} >Week</Button><Button className={style.button_inactive} onClick={() => setActiveTab("Month")}>Month</Button><Button className={style.button_inactive} onClick={() => setActiveTab("All")}>All</Button> </Stack> : <div className={style.tile_header}>{props.title}</div>
+
+  let reqPrice = props.type == "Bar" | props.type == "Info" | props.type == "Temp"? "" : <div className={style.tile_data}>${data?.tickers[0].converted_last.usd.toLocaleString()}</div>
+
   return (
-      <div className={`${style.basic_tile} ${props.size? style.large_tile : ""}`}>
-        <Stack spacing={1}>
-            <div className={style.tile_header}>{props.title}</div>
-            <div className={style.tile_data}>${data?.tickers[0].converted_last.usd.toLocaleString()}</div>
+    <div className={`${style.basic_tile} ${props.type? style.large_tile : ""}`}>
+        <Stack spacing={1} justifyContent="space-evenly">
+            {filter}
+            {reqPrice}
+            {test}
         </Stack>
-      </div>
+  </div>
   );
 }
 
@@ -26,8 +107,8 @@ const Panel = props => {
 
   props.titles.forEach((item, i) => {
     tilesList.push(
-    <Grid item key={i} xs={props.sizes[i]? 12 : 6} >
-        <Tile title={props.titles[i]} size={props.sizes[i]} />
+    <Grid item key={i} xs={props.types[i]? 12 : 6} >
+        <Tile title={props.titles[i]} type={props.types[i]} />
     </Grid>);
   });
 
@@ -38,24 +119,27 @@ const Panel = props => {
   );
 }
 
+const Temp = props => {
+  return null;
+}
+
 export const Metrics = props => {
 
   let buttonsList = [];
   let panel;
 
-  //tbd approach
   const [activeTab, setActiveTab] = useState(0);
   const handleChange = (event, newValue) => {
     setActiveTab(newValue);
   };
 
   props.buttons.forEach((item, i) => {
-    panel = activeTab == i ? <Panel key={i} titles={props.titles[i]} sizes={props.sizes[i]} /> : panel;
+    panel = activeTab == i ? <Panel key={i} titles={props.titles[i]} types={props.types[i]} /> : panel;
     buttonsList.push(
       <Button
         key={i}
         className={`${style.button_inactive} ${activeTab == i && style.button_active}`}
-        onClick={() => setActiveTab(i)}>
+        onClick={() => setActiveTab(i)} target="_blank" rel="noopener">
         {item}
       </Button>
     );
@@ -82,11 +166,11 @@ export async function getStaticProps() {
                 ['HISTORICAL ROOK APY', 'HISTORICAL SUPPLY/BORROW', 'CURRENT ROOK APY', 'TOTAL NUMBER OF VAULTS',
                 'DISTRIBUTED ROOK REWARDS', 'SAVED FROM LIQUIDATION BY JITU', 'TOTAL BORROW VOLUME', 'TOTAL SUPPLY VOLUME'],
                 ['HISTORICAL APY', 'TOTAL FEES COLLECTED', 'ROOK REWARDS DISTRIBUTED']];
-  let sizes = [[1,,,,], [,,,,1,1], [1,1,,], [1,1,,,,,,], [1,,]];
+  let types = [["Area",,,,], [,,,,"Temp","Temp"], ["Info","Bar",,], ["Bar","Area",,,,,,], ["Bar",,]];
 
   return {
-    props: {buttons,titles,sizes}
+    props: {buttons,titles,types}
   }
 }
 
-export default Metrics
+export default Metrics;
---
+++ src/sampleData.js
@@ -0,0 +1,649 @@
+const tvl =
+{
+  "updated": "2021-11-11T02:50:21.949487",
+  "total_tvl": 485827364,
+  "vaults": {
+    "total_tvl": 152837548,
+    "k_compound": {
+      "total_tvl": 152837548,
+      "assets": [
+        {
+          "name": "cDAI",
+          "total_tvl": "76418774.0"
+        },
+        {
+          "name": "cETH",
+          "total_tvl": "76418774.0"
+        }
+      ]
+    },
+    "k_aave": {
+      "total_tvl": 152837548,
+      "assets": [
+        {
+          "name": "cDAI",
+          "total_tvl": "76418774.0"
+        },
+        {
+          "name": "cETH",
+          "total_tvl": "76418774.0"
+        }
+      ]
+    }
+  },
+  "pools": {
+    "total_tvl": 332989816,
+    "assets": []
+  }
+}
+
+const hgVolumeWeek =
+{
+  "days": [
+    {
+      "updated": "2021-11-11T02:59:25.167075",
+      "daily_volume_usd": 7738286.7
+    },
+    {
+      "updated": "2021-11-10T02:59:25.167140",
+      "daily_volume_usd": 9686721
+    },
+    {
+      "updated": "2021-11-09T02:59:25.167159",
+      "daily_volume_usd": 3618272.4
+    },
+    {
+      "updated": "2021-11-08T02:59:25.167171",
+      "daily_volume_usd": 3655232.2
+    },
+    {
+      "updated": "2021-11-07T02:59:25.167183",
+      "daily_volume_usd": 3987033.8
+    },
+    {
+      "updated": "2021-11-06T02:59:25.167194",
+      "daily_volume_usd": 5667296.7
+    },
+    {
+      "updated": "2021-11-05T02:59:25.167206",
+      "daily_volume_usd": 2004061.5
+    }
+  ]
+}
+
+const hgVolumeMonth =
+{
+  "days": [
+    {
+      "updated": "2021-11-11T03:02:21.150095",
+      "daily_volume_usd": 7522545.9
+    },
+    {
+      "updated": "2021-11-10T03:02:21.150105",
+      "daily_volume_usd": 5043705.2
+    },
+    {
+      "updated": "2021-11-09T03:02:21.150113",
+      "daily_volume_usd": 6101532.9
+    },
+    {
+      "updated": "2021-11-08T03:02:21.150121",
+      "daily_volume_usd": 1823608.7
+    },
+    {
+      "updated": "2021-11-07T03:02:21.150128",
+      "daily_volume_usd": 2115165
+    },
+    {
+      "updated": "2021-11-06T03:02:21.150135",
+      "daily_volume_usd": 8122576.2
+    },
+    {
+      "updated": "2021-11-05T03:02:21.150143",
+      "daily_volume_usd": 7631570.9
+    },
+    {
+      "updated": "2021-11-04T03:02:21.150151",
+      "daily_volume_usd": 3743199.1
+    },
+    {
+      "updated": "2021-11-03T03:02:21.150158",
+      "daily_volume_usd": 9107415.5
+    },
+    {
+      "updated": "2021-11-02T03:02:21.150166",
+      "daily_volume_usd": 4689453.3
+    },
+    {
+      "updated": "2021-11-01T03:02:21.150173",
+      "daily_volume_usd": 2645933.8
+    },
+    {
+      "updated": "2021-10-31T03:02:21.150180",
+      "daily_volume_usd": 9274305.5
+    },
+    {
+      "updated": "2021-10-30T03:02:21.150188",
+      "daily_volume_usd": 4276437.2
+    },
+    {
+      "updated": "2021-10-29T03:02:21.150195",
+      "daily_volume_usd": 3077764.4
+    },
+    {
+      "updated": "2021-10-28T03:02:21.150203",
+      "daily_volume_usd": 1718887.9
+    },
+    {
+      "updated": "2021-10-27T03:02:21.150210",
+      "daily_volume_usd": 1768779.9
+    },
+    {
+      "updated": "2021-10-26T03:02:21.150217",
+      "daily_volume_usd": 5756109.9
+    },
+    {
+      "updated": "2021-10-25T03:02:21.150225",
+      "daily_volume_usd": 7724098.2
+    },
+    {
+      "updated": "2021-10-24T03:02:21.150232",
+      "daily_volume_usd": 6688993.9
+    },
+    {
+      "updated": "2021-10-23T03:02:21.150240",
+      "daily_volume_usd": 5571740
+    },
+    {
+      "updated": "2021-10-22T03:02:21.150247",
+      "daily_volume_usd": 3619216.8
+    },
+    {
+      "updated": "2021-10-21T03:02:21.150254",
+      "daily_volume_usd": 1289226.7
+    },
+    {
+      "updated": "2021-10-20T03:02:21.150261",
+      "daily_volume_usd": 6467970
+    },
+    {
+      "updated": "2021-10-19T03:02:21.150268",
+      "daily_volume_usd": 8934595.4
+    },
+    {
+      "updated": "2021-10-18T03:02:21.150276",
+      "daily_volume_usd": 3908485.9
+    },
+    {
+      "updated": "2021-10-17T03:02:21.150284",
+      "daily_volume_usd": 9012466.2
+    },
+    {
+      "updated": "2021-10-16T03:02:21.150291",
+      "daily_volume_usd": 9964609.5
+    },
+    {
+      "updated": "2021-10-15T03:02:21.150298",
+      "daily_volume_usd": 2698038.3
+    },
+    {
+      "updated": "2021-10-14T03:02:21.150305",
+      "daily_volume_usd": 8692234.8
+    },
+    {
+      "updated": "2021-10-13T03:02:21.150313",
+      "daily_volume_usd": 8570531.7
+    }
+  ]
+}
+
+const hgVolumeAll =
+{
+  "days": [
+    {
+      "updated": "2021-11-11T03:04:24.295435",
+      "daily_volume_usd": 9659455.1
+    },
+    {
+      "updated": "2021-11-10T03:04:24.295451",
+      "daily_volume_usd": 7474705.5
+    },
+    {
+      "updated": "2021-11-09T03:04:24.295463",
+      "daily_volume_usd": 6325308.4
+    },
+    {
+      "updated": "2021-11-08T03:04:24.295473",
+      "daily_volume_usd": 7339990.1
+    },
+    {
+      "updated": "2021-11-07T03:04:24.295484",
+      "daily_volume_usd": 6968580.4
+    },
+    {
+      "updated": "2021-11-06T03:04:24.295495",
+      "daily_volume_usd": 6088988.2
+    },
+    {
+      "updated": "2021-11-05T03:04:24.295506",
+      "daily_volume_usd": 4231362
+    },
+    {
+      "updated": "2021-11-04T03:04:24.295516",
+      "daily_volume_usd": 2482751.9
+    },
+    {
+      "updated": "2021-11-03T03:04:24.295527",
+      "daily_volume_usd": 2545757
+    },
+    {
+      "updated": "2021-11-02T03:04:24.295538",
+      "daily_volume_usd": 3747355.4
+    },
+    {
+      "updated": "2021-11-01T03:04:24.295548",
+      "daily_volume_usd": 6946650.4
+    },
+    {
+      "updated": "2021-10-31T03:04:24.295560",
+      "daily_volume_usd": 8019328
+    },
+    {
+      "updated": "2021-10-30T03:04:24.295571",
+      "daily_volume_usd": 1179734
+    },
+    {
+      "updated": "2021-10-29T03:04:24.295582",
+      "daily_volume_usd": 1973292.4
+    },
+    {
+      "updated": "2021-10-28T03:04:24.295594",
+      "daily_volume_usd": 2121068.2
+    },
+    {
+      "updated": "2021-10-27T03:04:24.295605",
+      "daily_volume_usd": 3341653.3
+    },
+    {
+      "updated": "2021-10-26T03:04:24.295617",
+      "daily_volume_usd": 9743239.1
+    },
+    {
+      "updated": "2021-10-25T03:04:24.295629",
+      "daily_volume_usd": 9015349.4
+    },
+    {
+      "updated": "2021-10-24T03:04:24.295641",
+      "daily_volume_usd": 6321036.6
+    },
+    {
+      "updated": "2021-10-23T03:04:24.295653",
+      "daily_volume_usd": 1798518.2
+    },
+    {
+      "updated": "2021-10-22T03:04:24.295666",
+      "daily_volume_usd": 3161235.9
+    },
+    {
+      "updated": "2021-10-21T03:04:24.295679",
+      "daily_volume_usd": 1709408.4
+    },
+    {
+      "updated": "2021-10-20T03:04:24.295691",
+      "daily_volume_usd": 6523017.9
+    },
+    {
+      "updated": "2021-10-19T03:04:24.295703",
+      "daily_volume_usd": 7784045
+    },
+    {
+      "updated": "2021-10-18T03:04:24.295716",
+      "daily_volume_usd": 8287559.3
+    },
+    {
+      "updated": "2021-10-17T03:04:24.295729",
+      "daily_volume_usd": 5695051.4
+    },
+    {
+      "updated": "2021-10-16T03:04:24.295741",
+      "daily_volume_usd": 7224234.2
+    },
+    {
+      "updated": "2021-10-15T03:04:24.295752",
+      "daily_volume_usd": 1105443.7
+    },
+    {
+      "updated": "2021-10-14T03:04:24.295771",
+      "daily_volume_usd": 1070329.4
+    },
+    {
+      "updated": "2021-10-13T03:04:24.295784",
+      "daily_volume_usd": 7863028.3
+    },
+    {
+      "updated": "2021-10-12T03:04:24.295796",
+      "daily_volume_usd": 9162172.1
+    },
+    {
+      "updated": "2021-10-11T03:04:24.295808",
+      "daily_volume_usd": 3514046
+    },
+    {
+      "updated": "2021-10-10T03:04:24.295820",
+      "daily_volume_usd": 7290142.9
+    },
+    {
+      "updated": "2021-10-09T03:04:24.295833",
+      "daily_volume_usd": 4409997.1
+    },
+    {
+      "updated": "2021-10-08T03:04:24.295845",
+      "daily_volume_usd": 9114705.7
+    },
+    {
+      "updated": "2021-10-07T03:04:24.295857",
+      "daily_volume_usd": 5627103
+    },
+    {
+      "updated": "2021-10-06T03:04:24.295868",
+      "daily_volume_usd": 1388913.8
+    },
+    {
+      "updated": "2021-10-05T03:04:24.295880",
+      "daily_volume_usd": 2498089.6
+    },
+    {
+      "updated": "2021-10-04T03:04:24.295891",
+      "daily_volume_usd": 6857620.5
+    },
+    {
+      "updated": "2021-10-03T03:04:24.295903",
+      "daily_volume_usd": 5877856
+    },
+    {
+      "updated": "2021-10-02T03:04:24.295914",
+      "daily_volume_usd": 9257130.9
+    },
+    {
+      "updated": "2021-10-01T03:04:24.295927",
+      "daily_volume_usd": 4818090.6
+    },
+    {
+      "updated": "2021-09-30T03:04:24.295939",
+      "daily_volume_usd": 9265295.1
+    },
+    {
+      "updated": "2021-09-29T03:04:24.295951",
+      "daily_volume_usd": 9216601.8
+    },
+    {
+      "updated": "2021-09-28T03:04:24.295963",
+      "daily_volume_usd": 9712929.3
+    },
+    {
+      "updated": "2021-09-27T03:04:24.295977",
+      "daily_volume_usd": 4606198.5
+    },
+    {
+      "updated": "2021-09-26T03:04:24.295990",
+      "daily_volume_usd": 5653754.7
+    },
+    {
+      "updated": "2021-09-25T03:04:24.296003",
+      "daily_volume_usd": 3075898
+    },
+    {
+      "updated": "2021-09-24T03:04:24.296015",
+      "daily_volume_usd": 8313085.2
+    },
+    {
+      "updated": "2021-09-23T03:04:24.296028",
+      "daily_volume_usd": 8899189.9
+    },
+    {
+      "updated": "2021-09-22T03:04:24.296041",
+      "daily_volume_usd": 5624409.7
+    },
+    {
+      "updated": "2021-09-21T03:04:24.296054",
+      "daily_volume_usd": 7918873.3
+    },
+    {
+      "updated": "2021-09-20T03:04:24.296067",
+      "daily_volume_usd": 5332624.6
+    },
+    {
+      "updated": "2021-09-19T03:04:24.296081",
+      "daily_volume_usd": 7837479.9
+    },
+    {
+      "updated": "2021-09-18T03:04:24.296094",
+      "daily_volume_usd": 2687273.7
+    },
+    {
+      "updated": "2021-09-17T03:04:24.296107",
+      "daily_volume_usd": 6610060.6
+    },
+    {
+      "updated": "2021-09-16T03:04:24.296120",
+      "daily_volume_usd": 7624502.4
+    },
+    {
+      "updated": "2021-09-15T03:04:24.296133",
+      "daily_volume_usd": 3542872.3
+    },
+    {
+      "updated": "2021-09-14T03:04:24.296146",
+      "daily_volume_usd": 1056718.4
+    },
+    {
+      "updated": "2021-09-13T03:04:24.296159",
+      "daily_volume_usd": 1582282.9
+    },
+    {
+      "updated": "2021-09-12T03:04:24.296172",
+      "daily_volume_usd": 1104208.5
+    },
+    {
+      "updated": "2021-09-11T03:04:24.296185",
+      "daily_volume_usd": 4257695.8
+    },
+    {
+      "updated": "2021-09-10T03:04:24.296198",
+      "daily_volume_usd": 9996531
+    },
+    {
+      "updated": "2021-09-09T03:04:24.296211",
+      "daily_volume_usd": 4089770
+    },
+    {
+      "updated": "2021-09-08T03:04:24.296224",
+      "daily_volume_usd": 7525517.7
+    },
+    {
+      "updated": "2021-09-07T03:04:24.296238",
+      "daily_volume_usd": 7759199.9
+    },
+    {
+      "updated": "2021-09-06T03:04:24.296251",
+      "daily_volume_usd": 2745827.5
+    },
+    {
+      "updated": "2021-09-05T03:04:24.296263",
+      "daily_volume_usd": 3369019.8
+    },
+    {
+      "updated": "2021-09-04T03:04:24.296277",
+      "daily_volume_usd": 2296829.5
+    },
+    {
+      "updated": "2021-09-03T03:04:24.296289",
+      "daily_volume_usd": 8081422.9
+    },
+    {
+      "updated": "2021-09-02T03:04:24.296303",
+      "daily_volume_usd": 1641498.7
+    },
+    {
+      "updated": "2021-09-01T03:04:24.296315",
+      "daily_volume_usd": 3984415.9
+    },
+    {
+      "updated": "2021-08-31T03:04:24.296328",
+      "daily_volume_usd": 6002149
+    },
+    {
+      "updated": "2021-08-30T03:04:24.296341",
+      "daily_volume_usd": 7108295.3
+    },
+    {
+      "updated": "2021-08-29T03:04:24.296354",
+      "daily_volume_usd": 6901952.6
+    },
+    {
+      "updated": "2021-08-28T03:04:24.296367",
+      "daily_volume_usd": 2467179.2
+    },
+    {
+      "updated": "2021-08-27T03:04:24.296379",
+      "daily_volume_usd": 3098967.6
+    },
+    {
+      "updated": "2021-08-26T03:04:24.296393",
+      "daily_volume_usd": 6825225
+    },
+    {
+      "updated": "2021-08-25T03:04:24.296406",
+      "daily_volume_usd": 1095059.9
+    },
+    {
+      "updated": "2021-08-24T03:04:24.296418",
+      "daily_volume_usd": 2470584.2
+    },
+    {
+      "updated": "2021-08-23T03:04:24.296431",
+      "daily_volume_usd": 9716004
+    },
+    {
+      "updated": "2021-08-22T03:04:24.296442",
+      "daily_volume_usd": 3069973
+    },
+    {
+      "updated": "2021-08-21T03:04:24.296453",
+      "daily_volume_usd": 3257812.5
+    },
+    {
+      "updated": "2021-08-20T03:04:24.296463",
+      "daily_volume_usd": 7927646.4
+    },
+    {
+      "updated": "2021-08-19T03:04:24.296474",
+      "daily_volume_usd": 1665058.3
+    },
+    {
+      "updated": "2021-08-18T03:04:24.296484",
+      "daily_volume_usd": 4508657.1
+    },
+    {
+      "updated": "2021-08-17T03:04:24.296495",
+      "daily_volume_usd": 2871325
+    },
+    {
+      "updated": "2021-08-16T03:04:24.296506",
+      "daily_volume_usd": 4140406.7
+    },
+    {
+      "updated": "2021-08-15T03:04:24.296518",
+      "daily_volume_usd": 4084310.9
+    },
+    {
+      "updated": "2021-08-14T03:04:24.296528",
+      "daily_volume_usd": 9538178.7
+    },
+    {
+      "updated": "2021-08-13T03:04:24.296547",
+      "daily_volume_usd": 1086724.6
+    },
+    {
+      "updated": "2021-08-12T03:04:24.296560",
+      "daily_volume_usd": 4912558.7
+    },
+    {
+      "updated": "2021-08-11T03:04:24.296570",
+      "daily_volume_usd": 2240775.2
+    },
+    {
+      "updated": "2021-08-10T03:04:24.296582",
+      "daily_volume_usd": 4008526.6
+    },
+    {
+      "updated": "2021-08-09T03:04:24.296593",
+      "daily_volume_usd": 4030844
+    },
+    {
+      "updated": "2021-08-08T03:04:24.296603",
+      "daily_volume_usd": 3258547.8
+    },
+    {
+      "updated": "2021-08-07T03:04:24.296613",
+      "daily_volume_usd": 2841780.3
+    },
+    {
+      "updated": "2021-08-06T03:04:24.296624",
+      "daily_volume_usd": 1502163
+    },
+    {
+      "updated": "2021-08-05T03:04:24.296635",
+      "daily_volume_usd": 9427652.2
+    },
+    {
+      "updated": "2021-08-04T03:04:24.296648",
+      "daily_volume_usd": 8094421.7
+    }
+  ]
+}
+
+const kTokens =
+{
+  "ktokens": [
+    {
+      "block_updated": 13592408,
+      "name": "kETH",
+      "address": "0x179212cb86D0eE6A4dfb2AbB1CF6A09feE0A9525",
+      "underlying_value": 0.9680087131257022,
+      "usd_value": 4462.9364112561325
+    },
+    {
+      "block_updated": 13592408,
+      "name": "kwETH",
+      "address": "0x834CAcd6425fA6c7126b028B3d1E4cda53EB7257",
+      "underlying_value": 1.0016826604978417,
+      "usd_value": 4618.187788439065
+    },
+    {
+      "block_updated": 13592408,
+      "name": "kUSDC",
+      "address": "0x3045312Fb54f00f43d6607999e387Db58FFb4cF4",
+      "underlying_value": 0.9943676485709091,
+      "usd_value": 0.9943676485709091
+    },
+    {
+      "block_updated": 13592408,
+      "name": "kDAI",
+      "address": "0x8EE17Fa30D63ebD66e02205B1DF2f30D60a5CA30",
+      "underlying_value": 0.9989191013074699,
+      "usd_value": 0.9989191013074699
+    },
+    {
+      "block_updated": 13592408,
+      "name": "kBTC",
+      "address": "0x77565202D78a6EDA565c7DC737FF1d8E64fd672a",
+      "underlying_value": 1.0035384030899166,
+      "usd_value": 64883.77545177856
+    }
+  ]
+}
+
+
+
+export {tvl, hgVolumeWeek, hgVolumeMonth, hgVolumeAll, kTokens};
