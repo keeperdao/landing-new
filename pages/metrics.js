eb10a511891af8aa01ea3e74a35e8692ab002cda
92233500+leveredlong <92233500+leveredlong@users.noreply.github.com>
2021-11-14 13:35:25 -0800

    chart customization + misc

--- pages/metrics.js
+++ pages/metrics.js
@@ -1,99 +1,184 @@
-import React, {useState} from 'react';
-import { Grid, Button, Stack } from "@material-ui/core";
+import React, {useState, useCallback, useMemo, useEffect} from 'react';
+import { Grid, Button, Stack, Box, Chip, Link, Select, MenuItem, ButtonGroup, Tab, Tabs, RadioGroup, FormControlLabel, Radio} from "@material-ui/core";
 import useSWR from 'swr'
 import style from '../src/dev.module.css'
-import { AreaChart, Area, XAxis, Tooltip, ResponsiveContainer, BarChart, Bar} from 'recharts';
-import {tvl, hgVolumeWeek, hgVolumeMonth, hgVolumeAll, kTokens} from '../src/sampleData'
+import { AreaChart, Area, XAxis, Tooltip, ResponsiveContainer, BarChart, Bar, Label, LabelList, Brush} from 'recharts';
+import {tvl, hgVolumeWeek, hgVolumeMonth, hgVolumeAll, kTokens} from '../src/sampleData';
 
-
-//major refactor req
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const CustomTooltip = ({ active, payload, label }) => {
   if (active && payload && payload.length) {
-    console.log(payload)
     return (
-      <div className="custom-tooltip">
-        <p className="label">{`${label} : ${payload[0].value}`}</p>
+      <div className={`${style.ToolTip} ${style.tile_header}`}>
+        <p className={style.label}>{"$ " + `${payload[0].value}`}</p>
+        <p className={style.label}>{`${label}`}</p>
       </div>
     );
   }
-
   return null;
 };
 
+// <p className="label">{`${label} ${payload[0].value}`}</p>
+
+
 const AChart = props => {
 
-  const ChartType = props.type;
+  let lg = {
+    id: 'gradient',
+    x1: '0',
+    y1: '0',
+    x2: '0',
+    y2: '100%',
+    gradientUnits: "userSpaceOnUse",
+  }
 
-  const dataSet = props.data == "Week" ? hgVolumeWeek.days: props.data == "Month" ? hgVolumeMonth.days : hgVolumeAll.days
+  let xaxis ={
+    dataKey: "updated",
+    tickLine: "false",
+    tickSize: 0,
+    tickMargin: 10,
+    interval: `${props.data?.length}` - 2,
+    width: 1
+  }
 
+  let chart = {
+    data: props.data
+  }
+
+  let fill = {
+    type: "monotone",
+    dataKey: "daily_volume_usd",
+    stroke: "#6C46D6",
+    fill: "url(#gradient)",
+    fillOpacity: 1,
+    strokeWidth: 2
+  }
+
+  let tooltip = {
+    cursor: false,
+    position: {x: 'auto', y: -75},
+    content: <CustomTooltip />
+  }
 
   return (
+    <>
       <div style={{ width: "100%", height: 300 }}>
         <ResponsiveContainer>
-          <AreaChart data={dataSet} >
+          <AreaChart {...chart}>
             <defs>
-              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="30%" stopColor="#6C46D6" stopOpacity={0.5}/>
-                <stop offset="85%" stopColor="#6C46D60D" stopOpacity={0}/>
+              <linearGradient {...lg}>
+                <stop offset="0.2" stopColor="#6C46D6" />
+                <stop offset="0.9" stopColor="#6C46D60D" />
               </linearGradient>
             </defs>
-            <XAxis dataKey="name" />
-            <Tooltip  />
-            <Area type="monotone" dataKey="daily_volume_usd" stroke="#8884d8" fillOpacity={1} fill="url(#colorPv)" />
+            <XAxis {...xaxis}/>
+            <Tooltip {...tooltip} />
+            <Area {...fill}/>
           </AreaChart>
         </ResponsiveContainer>
       </div>
+      </>
     );
 }
 
+// <Brush dataKey='daily_volume_usd' height={30} stroke="#8884d8"/>
+
 const BChart = props => {
 
-  const ChartType = props.type;
+  let lg = {
+    id: 'gradient',
+    x1: '0',
+    y1: '0',
+    x2: '0',
+    y2: '100%',
+    gradientUnits: "userSpaceOnUse",
+  }
 
-  const dataSet = props.data == "Week" ? hgVolumeWeek.days: props.data == "Month" ? hgVolumeMonth.days : hgVolumeAll.days
+  let xaxis ={
+    dataKey: "updated",
+    tickLine: "false",
+    tickSize: 0,
+    tickMargin: 10,
+    interval: `${props.data?.length}` - 2,
+    width: 1
+  }
+
+  let chart = {
+    barCategoryGap: 2,
+    data: props.data
+  }
+
+  let fill = {
+    type: "monotone",
+    dataKey: "daily_volume_usd",
+    stroke: "url(#gradient)",
+    fill: "url(#gradient)",
+    fillOpacity: 0.5,
+    strokeWidth: 2
+  }
+
+  let tooltip = {
+    cursor: false,
+    position: {x: 'auto', y: -75},
+    content: <CustomTooltip />
+  }
 
   return (
-      <div style={{ width: "100%", height: 300 }}>
+      <div style={{ width: "100%", height: 300}}>
         <ResponsiveContainer>
-          <BarChart data={dataSet} >
+          <BarChart {...chart}>
             <defs>
-              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="30%" stopColor="#6C46D6" stopOpacity={0.5}/>
-                <stop offset="85%" stopColor="#6C46D60D" stopOpacity={0}/>
+              <linearGradient {...lg}>
+                <stop offset="0.2" stopColor="#6C46D6" />
+                <stop offset="0.9" stopColor="#6C46D60D" />
               </linearGradient>
             </defs>
-            <XAxis dataKey="name" />
-            <Tooltip  />
-            <Bar type="monotone" dataKey="daily_volume_usd" stroke="#8884d8" fillOpacity={1} fill="url(#colorPv)" />
+            <Tooltip {...tooltip} />
+            <XAxis {...xaxis}/>
+            <Bar {...fill} />
           </BarChart>
         </ResponsiveContainer>
       </div>
     );
 }
 
+
+const testingState = state => {
+    console.log(state);
+}
+
+// ticks={[props.data[1].updated, props.data[props.data.length-1].updated]}
+
 const Tile = props => {
 
+  console.log("rendering Tile: " + props?.title)
+
+
+  // let data;
   const {data} = useSWR('https://api.coingecko.com/api/v3/coins/rook/tickers?exchange_ids=uniswap_v2', fetcher)
 
-  const [activeTab, setActiveTab] = useState("Week");
-  const handleChange = (event, newValue) => {
-    setActiveTab(newValue);
-  };
+  const [activeTab, setActiveTab] = useState(() => hgVolumeWeek.days);
+  const handleChange = useCallback(event => {
+    setActiveTab(hgVolumeMonth.days)
+  }, [setActiveTab])
+
+
 
   let numberUsers = "5,375"
   const text = <p className={style.info_data}>To date {numberUsers} users have saved Ξ302,5 in gas and earned 5,166 ROOK by using the Hiding Game for limit orders</p>
 
-  let test = props.type ? props.type == "Area" ? <AChart type={props.type} data={activeTab}/> : props.type == "Bar" ? <BChart type={props.type} data={activeTab} /> : props.type == "Info" ? text : "" : ""
-  let filter = props.type == "Area" | props.type == "Bar"? <Stack direction="row" spacing={1} justify-content="flex-end"> <div className={style.tile_header}>{props.title}</div> <Button className={style.button_inactive} onClick={() => setActiveTab("Week")} >Week</Button><Button className={style.button_inactive} onClick={() => setActiveTab("Month")}>Month</Button><Button className={style.button_inactive} onClick={() => setActiveTab("All")}>All</Button> </Stack> : <div className={style.tile_header}>{props.title}</div>
+  let test = props.type ? props.type == "Area" ? <><div className={style.ToolTipSpace}></div><AChart type={props.type} data={activeTab} /> </>: props.type == "Bar" ? <><div className={style.ToolTipSpace}></div><BChart type={props.type} data={activeTab} /></> : props.type == "Info" ? text :  ""
+ : ""
+  let filter = props.type == "Bar" | props.type == "Area" ? <Stack direction="row" className={style.outer_stack}><div className={style.tile_header}>{props.title}</div><Stack direction="row" spacing={1} justifyContent="flex-end" className={style.test}> <span className={style.mini_button} onClick={() => setActiveTab(hgVolumeWeek.days)}>WEEK</span><span className={style.mini_button} onClick={() => setActiveTab(hgVolumeMonth.days)}>MONTH</span><span className={style.mini_button} onClick={() => setActiveTab(hgVolumeAll.days)}>ALL</span> </Stack></Stack> : <div className={style.tile_header}>{props.title}</div>
 
-  let reqPrice = props.type == "Bar" | props.type == "Info" | props.type == "Temp"? "" : <div className={style.tile_data}>${data?.tickers[0].converted_last.usd.toLocaleString()}</div>
+  let reqPrice = props.type == "Bar" | props.type == "Info" | props.type == "Temp"? "" : <div className={style.tile_data}>${data?.tickers[0]?.converted_last.usd?.toLocaleString()}</div>
 
   return (
-    <div className={`${style.basic_tile} ${props.type? style.large_tile : ""}`}>
+
+    <div className={`${style.basic_tile} ${props.type && style.large_tile}`}>
         <Stack spacing={1} justifyContent="space-evenly">
-            {filter}
+            <Box>{filter}</Box>
             {reqPrice}
             {test}
         </Stack>
@@ -103,14 +188,13 @@ const Tile = props => {
 
 const Panel = props => {
 
-  let tilesList = [];
+  // console.log("rendering Panel")
 
-  props.titles.forEach((item, i) => {
-    tilesList.push(
+  let tilesList = React.Children.map(props.titles ,(item, i) =>
     <Grid item key={i} xs={props.types[i]? 12 : 6} >
         <Tile title={props.titles[i]} type={props.types[i]} />
-    </Grid>);
-  });
+    </Grid>
+  );
 
   return (
       <Grid container spacing={0.5}>
@@ -119,37 +203,51 @@ const Panel = props => {
   );
 }
 
-const Temp = props => {
-  return null;
-}
 
 export const Metrics = props => {
 
-  let buttonsList = [];
-  let panel;
+  // console.log("rendering Metrics")
 
-  const [activeTab, setActiveTab] = useState(0);
-  const handleChange = (event, newValue) => {
-    setActiveTab(newValue);
-  };
+  const [activeTab, setActiveTab] = useState(() => 0);
+  const handleChange = useCallback((event, newValue) => {
+    setActiveTab(event?.target?.value);
+  }, [setActiveTab]);
 
-  props.buttons.forEach((item, i) => {
-    panel = activeTab == i ? <Panel key={i} titles={props.titles[i]} types={props.types[i]} /> : panel;
-    buttonsList.push(
-      <Button
-        key={i}
-        className={`${style.button_inactive} ${activeTab == i && style.button_active}`}
-        onClick={() => setActiveTab(i)} target="_blank" rel="noopener">
+  let b = {
+    onClick: handleChange,
+  }
+
+  let bg = {
+     disableRipple: true,
+  }
+
+  let p = {
+    titles: props.titles[activeTab],
+    types: props.types[activeTab]
+  }
+
+  let s1 = {
+    spacing: 4
+  }
+
+  let s2 = {
+    className: style.ButtonContainer,
+    direction: "row",
+    spacing: 1
+  }
+
+  let buttonList = useMemo(() => React.Children.map(props.buttons,(item, i) =>
+    <Button key={i} value={i} className={`${style.button_inactive} ${activeTab == i && style.button_active}`} {...b}>
         {item}
-      </Button>
-    );
-  });
+    </Button>
+  ), [activeTab]);
 
+  let panel = <Panel {...p} />
 
   return (
-      <Stack className={style.stack} spacing={4}>
-        <Stack className={style.group} direction="row" spacing={1}>
-          {buttonsList}
+      <Stack {...s1}>
+        <Stack {...s2}>
+          {buttonList}
         </Stack>
         {panel}
       </Stack>
@@ -166,10 +264,10 @@ export async function getStaticProps() {
                 ['HISTORICAL ROOK APY', 'HISTORICAL SUPPLY/BORROW', 'CURRENT ROOK APY', 'TOTAL NUMBER OF VAULTS',
                 'DISTRIBUTED ROOK REWARDS', 'SAVED FROM LIQUIDATION BY JITU', 'TOTAL BORROW VOLUME', 'TOTAL SUPPLY VOLUME'],
                 ['HISTORICAL APY', 'TOTAL FEES COLLECTED', 'ROOK REWARDS DISTRIBUTED']];
-  let types = [["Area",,,,], [,,,,"Temp","Temp"], ["Info","Bar",,], ["Bar","Area",,,,,,], ["Bar",,]];
+  let types = [["Area",,,,], [,,,,"Temp","Temp"], ["Info","Bar",,], ["Bar","Area",,,,,], ["Bar",,]];
 
   return {
-    props: {buttons,titles,types}
+    props: {buttons,titles, types}
   }
 }
 
